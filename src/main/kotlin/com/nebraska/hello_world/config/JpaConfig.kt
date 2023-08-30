95f83ff0698384e473f2aac106d4f0448667ef97
Igor julliano <igor.julliano2@gmail.com>
2023-08-30 17:14:46 -0300

    refactor: try to find env file on secrets folder

--- src/main/kotlin/com/nebraska/hello_world/config/JpaConfig.kt
+++ src/main/kotlin/com/nebraska/hello_world/config/JpaConfig.kt
@@ -1,5 +1,7 @@
 package com.nebraska.hello_world.config
 
+import io.github.cdimascio.dotenv.Dotenv
+import io.github.cdimascio.dotenv.DotenvException
 import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
@@ -11,23 +13,34 @@ import javax.sql.DataSource
 @Configuration
 @ConfigurationProperties("spring.datasource")
 class JpaConfig {
-    val dotEnv = dotenv()
-
     @Bean
     @Primary
     fun getDataSource(): DataSource {
-        return DataSourceBuilder.create()
-            .driverClassName("org.postgresql.Driver")
-            .url(getDataSourceUrl())
-            .username(dotEnv.get("DB_USERNAME"))
-            .password(dotEnv.get("DB_PASSWORD"))
-            .build()
-    }
+        var dotEnv: Dotenv;
+        try {
+            dotEnv = dotenv {
+                ignoreIfMissing = false
+                filename = ".env"
+            }
+        } catch (e: DotenvException) {
+            println("Env file not found. Trying to load env file from secrets folder (/etc/secrets)")
+            dotEnv = dotenv {
+                ignoreIfMissing = false
+                directory = "/etc/secrets/"
+                filename = ".env"
+            }
+        }
 
-    private fun getDataSourceUrl(): String {
+
+
         val host = dotEnv.get("DB_HOST")
         val dbName = dotEnv.get("DB_NAME")
 
-        return "jdbc:postgresql://$host/$dbName"
+        return DataSourceBuilder.create()
+            .driverClassName("org.postgresql.Driver")
+            .url("jdbc:postgresql://$host/$dbName")
+            .username(dotEnv.get("DB_USERNAME"))
+            .password(dotEnv.get("DB_PASSWORD"))
+            .build()
     }
 }
